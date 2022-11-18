6be12fae603689521730898365123a0904525571
Sam Haycraft <samuel.g.haycraft@xcelenergy.com>
2022-11-17 23:14:48 -0700

    feat:  adds new thingy

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ provider "aws" {
 
 resource "aws_key_pair" "sam_test_new_key" {
   key_name   = "sam_test_new_key"
-  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDK0yEgSJyau/533a1nlAusY1YbFzk5MKW8/lFAaO596tepCz51/k1N1J3mHMCi+6hQGhYoTtgsg8/ZG9LhXh7541NI5R9wwzbNcmk5AAIgMlU9hhUynjLYmr3HDidXinAw8akZ8z4QTiXaie+ui8eovUcFDbldEA5Z3iO6M0b4XxP1wJAfCAYpbUmsRfNZV+w5yZuwuze+wQbkAJh6eo64/hl+BHJiLOA0hyYvs8dW3O/8dks9wMw+9Y+L51SS0hswdvk8me9knlNDQ9RC/COfF9VSOcGPCk2qcvE5fMPmVeGLe8S+z/imqV57u4JhXsN8eMWtwEwz/awmNKoU7erNKLwR6s62EurKaR4vg/eQoqaCZr+B9VRZ7lk/lIlrh7T/tPqAXciZw+Tc2xSKW8SmyEIiwbbPlHiDz0ENtYC8gGQOO9nbKhSHj07pM1fiZxY6EP0S45KWLQt8xnUdBw/aQX6KED+WunUu4DJP1Xhzfqghc122EJNZtMv5PrDnWfM= shaycraft@MacBook-Pro.local"
+  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCzIp5hD3rNeWZ0PIpZX0jfrZ/Zhs58fcnQspmhe+vAh7ya6SYPVFSX0uTA9baED6cNFgm+POwR0V9B73duxgcG48sXLOTRqOEm+gOJwxA52cXsSv/LYj6FwWMXJlBzreg7QUlsx61Jqrb8zYXNiERv9buNj91QP8VICJZx1liPQiy3dkOMB6W+fr21rbUtZjZSsb/j/M8K7WtA+WDP5J7M4YFeIfOUejR3D1/79jMOgae0+AtkMe4b2Ln+7+AsPcRfOChkZjCBKEodtL1o45afBAqQDZoknCbY4vtyrBD3KHrOhzvIBAPkgRIAWbfD3SSzEIooFKPP4Xiq7L9o8S2N shaycraft@Samuels-MacBook-Pro.local"
 }
 
 resource "aws_instance" "SshKeyTest" {
@@ -21,7 +21,7 @@ resource "aws_instance" "SshKeyTest" {
     type        = "ssh"
     host        = self.public_ip
     user        = "ubuntu"
-    private_key = file("/Users/shaycraft/.ssh/id_rsa")
+    private_key = file("/Users/shaycraft/.ssh/id_rsa_aws_terraform_tutorial")
     timeout     = "4m"
   }
 }
